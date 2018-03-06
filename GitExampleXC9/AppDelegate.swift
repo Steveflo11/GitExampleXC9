e72a119b0551c3d133299f333f61cfbc9768244d
Steve Flores <Steve@thatsbyers.com>
2018-03-06 10:59:40 -0800

    Initial Commit

---
+++ GitExampleXC9/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitExampleXC9
+//
+//  Created by Byers on 3/6/18.
+//  Copyright © 2018 Byers. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
